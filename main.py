dd9e45e3355d0a7909b23b18eb56e6a87b09dc25
abhinav paturi <43619861+abhinavpaturi@users.noreply.github.com>
2023-04-04 19:17:14 +0000

    changed the model value to text-davinci-003, funnily enough gpt4 based chat gpt got that part wrong. Fixed multiple instances of indexing errors. Skips bots turn if its the first phrase of the story.

--- main.py
+++ main.py
@@ -1,50 +1,42 @@
-import spacy
-import random
+import openai
+import re
 
-# Load the English language model for spaCy
-nlp = spacy.load("en_core_web_sm")
-
-# Define a dictionary of prompts to continue the story
-prompts = {
-  "treasures": [
-    "They found a chest full of gold coins.",
-    "They discovered a map leading to a hidden treasure."
-  ],
-  "foes": [
-    "They battled a group of fierce pirates.",
-    "They encountered a giant octopus."
-  ],
-  "seas": [
-    "They braved a raging storm.",
-    "They navigated through a treacherous maze of rocks."
-  ],
-  "pirates": [
-    "They fought their way through a horde of undead pirates.",
-    "They narrowly escaped a pirate ambush."
-  ]
-}
+# Initialize OpenAI API client
+openai.api_key = ("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")
 
 
-# Define a function to generate a response based on user input
-def generate_response(user_input):
-  # Process the user input with spaCy
-  doc = nlp(user_input)
-  # Check if the user input contains a prompt keyword
-  for token in doc:
-    if token.text.lower() in prompts.keys():
-      # Randomly select a response from the prompts dictionary
-      response = random.choice(prompts[token.text.lower()])
-      return response
-  # If no prompt keyword is found, generate a default response
-  return "Interesting story! What happens next?"
+def get_story_continuation(prompt, tokens=30):
+  response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompt,
+    n=1,
+    max_tokens=tokens,
+    temperature=0.7,
+  )
+  continuation = response.choices[0].text.strip()
+  return re.sub(r"\n+", " ", continuation)
 
 
-# Start the conversation
-print("Let's create a story together! Tell me what happens next.")
-while True:
-  # Get user input
-  user_input = input("You: ")
-  # Generate a response
-  response = generate_response(user_input)
-  # Print the response
-  print("Bot: " + response)
+def short_story_teller():
+  print("Welcome to the short story teller!")
+  turns = int(input("How many turns? "))
+
+  story = []
+  for turn in range(turns):
+    if turn % 2 == 0:
+      user_input = input("\nUser: ")
+      story.append(user_input)
+      if turn == 0:
+        continue
+    else:
+      prompt = f"{story[-1]}"
+      bot_input = get_story_continuation(prompt)
+      print(f"Bot: {bot_input}")
+      story.append(bot_input)
+
+  full_story = " ".join(story)
+  print(f"\nThe short story is:\n{full_story}")
+
+
+if __name__ == "__main__":
+  short_story_teller()
